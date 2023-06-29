44923296ba2e8a414040f1a3aa2ca24651c334cd
Milton Giacomini Neto <milton.jneto@mercadolivre.com>
2023-06-29 18:27:17 -0300

    Day 2 - Transforma resposta em struct

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
+
+type Items struct {
+	Items []Filme `json:"items"`
+}
+
+type Filme struct {
+	Titulo         string `json:"title"`
+	TituloCompleto string `json:"fullTitle"`
+	Ano            string `json:"year"`
+	Nota           string `json:"imDbRating"`
+	Imagem         string `json:"image"`
+	Elenco         string `json:"crew"`
+	Ranking        string `json:"rank"`
+}
 
 func main() {
 	print("Bem vinda(o) ao sistema de busca de filmes!")
@@ -13,7 +28,7 @@ func main() {
 	GetTop250Movies()
 }
 
-func GetTop250Movies() {
+func GetTop250Movies() []Filme {
 	res, err := http.Get("https://imdb-api.com/en/API/Top250Movies/{apiKey}")
 	if err != nil {
 		fmt.Print(err.Error())
@@ -23,5 +38,9 @@ func GetTop250Movies() {
 	defer res.Body.Close()
 
 	resBody, _ := io.ReadAll(res.Body)
-	fmt.Print("Body: ", string(resBody))
+	filmes := Items{}
+
+	err = json.Unmarshal(resBody, &filmes)
+
+	return filmes.Items
 }
